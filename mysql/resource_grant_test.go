55de875fd05187bf7a5355fdfee9689a4c0eec5f
Zander Hill <zander@xargs.io>
2024-06-26 18:21:40 -0700

    Add back in test skips where it fails for TiDB 7.5.2
    These are skipped because of syntactical differences between mysql
    and TiDB. These tend to have mariadb/rds/tidb matching more closely
    in behavior.

--- mysql/resource_grant_test.go
+++ mysql/resource_grant_test.go
@@ -369,6 +369,7 @@ func TestAccGrant_roleToUser(t *testing.T) {
 			testAccPreCheck(t)
 			testAccPreCheckSkipRds(t)
 			testAccPreCheckSkipNotMySQLVersionMin(t, "8.0.0")
+			testAccPreCheckSkipTiDB(t)
 		},
 		ProviderFactories: testAccProviderFactories,
 		CheckDestroy:      testAccGrantCheckDestroy,
@@ -392,6 +393,7 @@ func TestAccGrant_complexRoleGrants(t *testing.T) {
 			testAccPreCheck(t)
 			testAccPreCheckSkipMariaDB(t)
 			testAccPreCheckSkipNotMySQLVersionMin(t, "8.0.0")
+			testAccPreCheckSkipTiDB(t)
 		},
 		ProviderFactories: testAccProviderFactories,
 		CheckDestroy:      testAccGrantCheckDestroy,
